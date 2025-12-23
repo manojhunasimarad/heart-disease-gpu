d28fabc59e333a8f88afa48db5764e395b465cf4
manojhunasimarad <manojsumya@gmail.com>
2025-12-23 18:15:26 +0530

    feat: add CUDA logistic regression stubs

--- src/cuda_logreg/host.cu
+++ src/cuda_logreg/host.cu
@@ -1,1 +1,9 @@
 // Host-side C++ code
+// src/cuda_logreg/host.cu
+#include <iostream>
+
+// TODO: allocate data, call CUDA kernels, compute accuracy
+int main() {
+    std::cout << "CUDA logistic regression stub\n";
+    return 0;
+}
--- src/cuda_logreg/kernel.cu
+++ src/cuda_logreg/kernel.cu
@@ -1,1 +1,5 @@
 // CUDA kernels
+// src/cuda_logreg/kernel.cu
+#include <cuda_runtime.h>
+
+// TODO: implement logistic regression kernels here
